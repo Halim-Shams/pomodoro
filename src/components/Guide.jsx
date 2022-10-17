e28f011bb5380ee2e7bdfb90a7aa3297232297ec
Halim-Shams <halimshams8@gmail.com>
2022-10-17 09:53:11 +0430

    Timmer added

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import Timmer from './Timmer';
 
 function Guide(props) {
 	return (
 		<div className='flex flex-col items-center justify-center gap-2'>
-			<h1 className='text-yellow-500 text-5xl font-bold dark:text-gray-200'>
-				{props.focus}
+			<h1 className='text-yellow-500 text-4xl font-bold dark:text-gray-200'>
+				{props.focus.toUpperCase()}
 			</h1>
 			<p className='text-gray-500 text-2xl font-light w-4/12 dark:text-gray-400'>
-				You have 25min to work on{' '}
-				<span className='font-medium'>{props.focus}</span>, When the time is up,
-				I'll inform you by playing a sound!
+				You have{' '}
+				<span className='text-yellow-500 font-medium font-mono'>25min</span> to
+				work on this task, When the time is up, I'll inform you by playing a
+				sound!
 			</p>
 			<h4 className='text-gray-500 font-bold dark:text-gray-400'>
 				Good Luck 🚀
 			</h4>
+			<Timmer />
 		</div>
 	);
 }
---
+++ src/components/Timmer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Timmer() {
+	return (
+		<div>
+			<h2 className='text-9xl text-yellow-400 font-mono font-extrabold dark:text-gray-200'>
+				25:00
+			</h2>
+		</div>
+	);
+}
+
+export default Timmer;
